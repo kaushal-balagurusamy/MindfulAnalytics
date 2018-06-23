f96cd7fb6f4c5e02ddd9598a6c9945294c393ab0
Abhijith Balagurusamy <crookedberet@gmail.com>
2018-06-22 23:46:59 -0400

    Finished Date Class

--- MindfulAnalyticsv1/src/TaskManager/Date.java
+++ MindfulAnalyticsv1/src/TaskManager/Date.java
@@ -1,13 +1,47 @@
 package TaskManager;
-
+import java.sql.SQLOutput;
+import java.time.*;
+import java.time.format.*;
 public class Date {
-    int month;
-    int day;
-    int year;
 
-    Date(int m, int d, int y) {
-        month = m;
-        day = d;
-        year = y;
+    int Year;
+    int Month;
+    int Day;
+    Date(int m, int y, int d) {
+        Year = y;
+        Month = m;
+        Day = d;
+    }
+
+
+    public static Date currentDate() {
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter DayFormat = DateTimeFormatter.ofPattern("dd");
+        DateTimeFormatter MonthFormat = DateTimeFormatter.ofPattern("MM");
+        DateTimeFormatter YearFormat = DateTimeFormatter.ofPattern("yyyy");
+        String Dtext = date.format(DayFormat);
+        String Mtext = date.format(MonthFormat);
+        String Ytext = date.format(YearFormat);
+        int day = Integer.parseInt(Dtext);
+        int month = Integer.parseInt(Mtext);
+        int year = Integer.parseInt(Ytext);
+        Date d = new Date(month, year, day);
+        return d;
+    }
+
+
+    public void printDate() {
+        System.out.print(Month);
+        System.out.print("/");
+        System.out.print(Day);
+        System.out.print("/");
+        System.out.print(Year);
+
+    }
+
+    public static void main(String args[]) {
+        Date c = currentDate();
+        c.printDate();
+
     }
 }
