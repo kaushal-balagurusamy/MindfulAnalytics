e3c603075b0c83b9de21363fb8752c71bdc3272a
Kaushal Balagurusamy <kaushal@Kaushals-MBP.home>
2018-06-24 14:50:01 -0400

    Event Class Description create

--- MindfulAnalyticsv1/src/TaskManager/Event.java
+++ MindfulAnalyticsv1/src/TaskManager/Event.java
@@ -1,5 +1,17 @@
 package TaskManager;
 
 public class Event {
-
+    /*
+    Event is distinct from a task!!!
+    This is for the team calendar management system
+    We will be able to have weekly views
+    Fields:
+        • Task
+        • Start Time
+        • End Time
+        •
+    Methods
+        • change span
+        • create subtask
+     */
 }
